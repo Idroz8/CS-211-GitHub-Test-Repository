bfeae2aa3fbe80c3125ba22bd08453cfc4c23072
basam.hassan <basam.hassan@1N005-TEACHER>
2025-02-18 11:46:38 -0500

    added largest element of array function

--- CS 211 GitHub Test Repostry.cpp
+++ CS 211 GitHub Test Repostry.cpp
@@ -10,6 +10,18 @@ int AddThree(int a, int b, int c) {
     return a + b + c;
 }
 
+int largest(int arr[], int size) {
+    int max = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
+    }
+
+    return max;
+}
+
 int main()
 {
     Hello();
